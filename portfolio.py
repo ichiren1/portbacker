c173575e162bb30de1fa1d896d48f3147e9f5581
Project20 <project20@Project20-no-MacBook-Air.local>
2013-07-22 17:10:57 +0900

    テンプレートに合わせたルーティングに設定、goalのGETを追加

--- portfolio.py
+++ portfolio.py
@@ -1,18 +1,29 @@
 import sys 
-from flask import Flask, request, redirect , url_for
+from flask import Flask, request, redirect, url_for, render_template
 
 app= Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def index_page():
-	return redirect('./static/top.html')
+	return render_template("top.html")
 
-@app.route('/goal_submit', methods=['POST'])
-def goal_page():
+@app.route('/goal', methods=['GET'])
+def goal_get():
+    return render_template("goal.html")
+
+@app.route('/goal', methods=['POST'])
+def goal_post():
 	goal_text = request.form['goal_text']
-	return "<html><body>%s</body></html>" % goal_text
+	return render_template("goal.html", goal_text=goal_text)
+
+@app.route('/portfolio')
+def portfolio():
+	return render_template("portfolio.html")
+
+@app.route('/test')
+def aaa():
+    return render_template("index.html", title="Flaski")
 
 if __name__ == '__main__':
 	app.debug = True
 	app.run()
-	
