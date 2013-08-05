ccc15585d3a9140b35c1dd2004d83c28504bcfba
Toshihiro Kamiya <kamiya@mbj.nifty.com>
2013-08-05 15:18:15 +0900

    インデントがスペースとタブ混じりなのを直した

--- portfolio.py
+++ portfolio.py
@@ -4,26 +4,26 @@ import sys
 from flask import Flask, request, redirect, url_for, render_template
 from pymongo import Connection
 
-app= Flask(__name__)
+app = Flask(__name__)
 
-#コネクション作成
+# コネクション作成
 con = Connection('localhost', 27017)
 
-#コネクションからtestデータベースを取得
+# コネクションからtestデータベースを取得
 db = con.portbacker
 
 # 以下のように記載することも可能
 # db = con['test']
 
-#testデータベースからfooコレクションを取得
+# testデータベースからfooコレクションを取得
 # col = db.portfolios
 
 # 以下のように記載することも可能
 # col = db['foo']
 
-@app.route('/',methods=['GET'])
+@app.route('/', methods=['GET'])
 def index_page():
-	return render_template("top.html")
+    return render_template("top.html")
 
 @app.route('/goal', methods=['GET'])
 def goal_get():
@@ -31,38 +31,36 @@ def goal_get():
 
 @app.route('/goal', methods=['POST'])
 def goal_post():
-	goal_text = request.form['goal_text']
-	return render_template("goal.html", goal_text=goal_text)
+    goal_text = request.form['goal_text']
+    return render_template("goal.html", goal_text=goal_text)
 
 @app.route('/portfolio')
 def portfolio():
-	return render_template("portfolio.html")
-
+    return render_template("portfolio.html")
 
 @app.route('/mongo', methods=['GET'])
 def mongo_get():
-  #testデータベースからfooコレクションを取得
-  col = db.portfolios
+    # testデータベースからfooコレクションを取得
+    col = db.portfolios
 
-  return render_template("mongo.html", db=db)
+    return render_template("mongo.html", db=db)
 
 @app.route('/mongo', methods=['POST'])
 def mongo_post():
-  col = db.portfolios
-  if request.form['button'] == u"設定":
-    if request.form['public'] == "true":
-      public = True
+    col = db.portfolios
+    if request.form['button'] == u"設定":
+        if request.form['public'] == "true":
+            public = True
+        else:
+            public = False
+        owner = request.form['owner']
+        text = request.form['text']
+
+        col.insert({"public":public, "owner":owner, "text":text})
     else:
-      public = False
-    owner = request.form['owner']
-    text = request.form['text']
-
-    col.insert({"public":public,"owner":owner,"text":text})
-  else:
-    col.remove({"owner":request.form['owner']})
-
-  
-  return render_template("mongo_post.html")
+        col.remove({"owner":request.form['owner']})
+    
+    return render_template("mongo_post.html")
 
 # print "========find_one========"
 # print col.find_one()
@@ -72,6 +70,5 @@ def mongo_post():
 #     print data
 
 if __name__ == '__main__':
-	app.debug = True
-	app.run()
-
+    app.debug = True
+    app.run()
