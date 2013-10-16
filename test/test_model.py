0f2d19b4cf43cdd6b73b9cacde9d80732c0fda07
b1012187 <b1012187@fun.ac.jp>
2013-10-17 00:17:28 +0900

    add tests of goal insertion and removal add tests of user initialize, insertion and find

--- test/test_model.py
+++ test/test_model.py
@@ -1,5 +1,9 @@
 #coding: utf-8
 
+import sys
+import os.path
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
+
 import unittest
 from pymongo import Connection
 
@@ -35,6 +39,24 @@ class ModelTest(unittest.TestCase):
         model.remove_goal_text(u, gt)
         goal_texts = model.get_goal_texts(u)
         self.assertFalse(gt in goal_texts)
+
+class UserTest(unittest.TestCase):
+    def test_init(self):
+        u = model.User("morohara", "b1012187", "高度ICT演習教育系", "情報システム", "B2")
+
+    def test_insert(self):
+        db = Connection('localhost', 27017).testdata
+        model.User.delete_all(db)        
+        u = model.User("morohara", "b1012187", "高度ICT演習教育系", "情報システム", "B2")
+        u.insert(db)
+
+    def test_find(self):
+        db = Connection('localhost', 27017).testdata
+        model.User.delete_all(db)
+        u = model.User("morohara", "b1012187", "高度ICT演習教育系", "情報システム", "B2")
+        u.insert(db)
+        act = model.User.find(db, "b1012187")
+        self.assertTrue(act != None)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
