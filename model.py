fe8223eb024488b0eabc24f735bcbe4ea77fa0e3
Toshihiro Kamiya <kamiya@mbj.nifty.com>
2013-10-16 16:45:47 +0900

    remove magic string literals

--- model.py
+++ model.py
@@ -4,6 +4,9 @@ from pymongo import Connection
 
 db = Connection('localhost', 27017).portbacker
 
+COL_GOALS = "goals"
+COL_PERSONALLOGS = "personallogs"
+
 def get_text_by_user_table_coumn(username, table, column):
     col = db[table]
     docs = col.find({"username": username})
@@ -12,26 +15,26 @@ def get_text_by_user_table_coumn(username, table, column):
     return texts
 
 def get_goal_texts(username):
-    goal_texts = get_text_by_user_table_coumn(username, "goals", "goal_text")
+    goal_texts = get_text_by_user_table_coumn(username, COL_GOALS, "goal_text")
     return goal_texts
 
 def remove_goal_text(username, goal_text):
-    col = db["goals"]
+    col = db[COL_GOALS]
     col.remove({"username": username, "goal_text": goal_text})
 
 def insert_goal_text(username, goal_text):
-    col = db["goals"]
+    col = db[COL_GOALS]
     col.insert({"username": username, "goal_text": goal_text})
 
 def get_log_texts(username):
-    log_texts = get_text_by_user_table_coumn(username, "personallogs", "personallog_text")
+    log_texts = get_text_by_user_table_coumn(username, COL_PERSONALLOGS, "personallog_text")
     return log_texts
 
 def remove_log_text(username, log_text):
-    col = db["personallogs"]
+    col = db[COL_PERSONALLOGS]
     col.remove({"username": username, "personallog_text": log_text})
 
 def insert_log_text(username, log_text):
-    col = db["personallogs"]
+    col = db[COL_PERSONALLOGS]
     col.insert({"username": username, "personallog_text": log_text})
 
